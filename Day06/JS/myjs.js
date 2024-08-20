88409ce829a2c71e146febff8fadc330183aac43
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-08-20 12:15:23 +0530

    Day06

---
+++ Day06/JS/myjs.js
@@ -0,0 +1,70 @@
+
+
+// for(let i=0;i<a.length;i++){
+//     console.log("value of array "+a[i]);
+// }
+
+// console.log(a);
+// console.log(a.join(" "));
+// console.log(a.join());
+let a=[4,5,6,7];
+//foreach 
+// a.forEach((myvalue,i,arr)=>{
+//     console.log("Data is "+myvalue+" Index is "+i+" Array is "+arr)
+// });
+
+let a1=[];
+// Push & POP
+// LIFO Principal
+// a1.push(10);
+// console.log(a1)
+// a1.push(20,50)
+// console.log(a1)
+// a1.push(100);
+// console.log(a1);
+// let res= a1.pop();
+// console.log(a1)
+// console.log("poped value is "+res)
+// a1.pop();
+// console.log(a1);
+
+// let a2=[40,45,85,79];
+// console.log(a2)
+// a2.splice(1,0,200);
+// console.log(a2)
+// a2.splice(2,0,90,209)
+// console.log(a2);
+// a2.splice(3,1);
+// console.log(a2);
+// a2.splice(2,2);
+// console.log(a2)
+
+// Map method 
+// element by element operation is done by map method 
+let a3=[2,3,4,5,6];
+
+// let squarr= a3.map((myvalue)=>{
+//         return (myvalue*myvalue);
+// });
+
+// console.log(`
+//         Original Array => ${a3}
+//         ===========================
+//         Square Array
+//         ${squarr}
+// `);
+
+
+function onSend(){
+    let inputBox= document.getElementById("myinput")
+    console.log("Data is "+inputBox.value);
+}
+
+
+
+
+
+
+
+
+
